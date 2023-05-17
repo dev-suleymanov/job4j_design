d7c36b7c50fe1182aebc1de5c852bd0e9927358e
dev-suleymanov <dev.suleymanov@gmail.com>
2023-05-17 19:53:13 +0300

    Что такое обобщенные типы (generics) [#4952 #416982]

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -32,21 +32,21 @@ public class Generics {
     public void printObject(List<?> list) {
         for (Iterator<?> iterator = list.iterator(); iterator.hasNext();) {
             Object next = iterator.next();
-            System.out.println("Текущий элемент: " + next);
+            System.out.println("Current element: " + next);
         }
     }
 
     public void printBoundedWildCard(List<? extends Predator> list) {
         for (Iterator<? extends Predator> iterator = list.iterator(); iterator.hasNext();) {
             Object next = iterator.next();
-            System.out.println("Текущий элемент: " + next);
+            System.out.println("Current element: " + next);
         }
     }
 
     public void printLowerBoundedWildCard(List<? super Predator> list) {
         for (Iterator<? super Predator> iterator = list.iterator(); iterator.hasNext();) {
             Object next = iterator.next();
-            System.out.println("Текущий элемент: " + next);
+            System.out.println("Current element: " + next);
         }
     }
 }
