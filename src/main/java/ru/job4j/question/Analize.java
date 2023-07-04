e7d608424ee4a0b44cb0ad56279a89b60160b206
dev-suleymanov <dev.suleymanov@gmail.com>
2023-07-04 21:55:19 +0300

    3. Статистика по коллекции. [#45889 # [#45889 #427717]]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -10,34 +10,21 @@ public class Analize {
         int added = 0;
         int changed = 0;
         int deleted = 0;
-        Map map = new HashMap();
+        Map<Integer, String> map = new HashMap();
         for (User user : current) {
             map.put(user.getId(), user.getName());
         }
         for (User user : previous) {
-            var value = map.get(user.getId());
-            if (value == null) {
+            String name = map.get(user.getId());
+            if (name == null) {
                 deleted++;
             } else {
-                if (!value.equals(user.getName())) {
+                if (!name.equals(user.getName())) {
                     changed++;
                 }
             }
         }
         added = current.size() - previous.size() + deleted;
         return new Info(added, changed, deleted);
-    }
-
-    public static void main(String[] args) {
-        Set<User> previous = new HashSet<>();
-        previous.add(new User(1, "a"));
-        previous.add(new User(2, "b"));
-        previous.add(new User(3, "c"));
-        Set<User> current = new HashSet<>();
-        current.add(new User(1, "a"));
-        current.add(new User(2, "b"));
-        current.add(new User(4, "e"));
-        Info result = diff(previous, current);
-        System.out.println(result);
     }
 }
