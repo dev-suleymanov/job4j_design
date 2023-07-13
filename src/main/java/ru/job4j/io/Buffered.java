a5949519c100c48bf5f752d9482a5141300f7f28
dev-suleymanov <dev.suleymanov@gmail.com>
2023-07-13 22:52:45 +0300

    5.0. Буферизированные потоки [#504979 # [#504979]]

---
+++ src/main/java/ru/job4j/io/Buffered.java
@@ -0,0 +1,15 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Buffered {
+    public static void main(String[] args) {
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream("data/input.txt"));
+             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("data/output.txt", true))) {
+            byte[] bytes = in.readAllBytes();
+            out.write(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
