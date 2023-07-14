5f9321bda08bd65d39f79c25e9ccf09a5101c3a9
dev-suleymanov <dev.suleymanov@gmail.com>
2023-07-14 13:14:13 +0300

    5.2. BufferedOutputStream [#252490 # [#252490]]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,8 +2,6 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class LogFilter {
