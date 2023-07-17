2665697633147a1e5db96aa2410c52d44afbfdee
dev-suleymanov <dev.suleymanov@gmail.com>
2023-07-17 20:41:41 +0300

    7. Анализ доступности сервера [#859 # [#859 #430483]]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,23 +8,23 @@ public class Analysis {
         String[] result = new String[2];
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(new FileWriter(target))) {
-            reader.lines()
-                    .forEach(line -> {
-                        String[] temp = line.split(" ");
-                        String status = temp[0];
-                        String data = temp[1];
-                        if (Objects.equals(status, "400") || Objects.equals(status, "500")) {
-                            if (Objects.isNull(result[0])) {
-                                result[0] = data;
-                            }
-                        } else {
-                            if (Objects.nonNull(result[0])) {
-                                result[1] = data;
-                                writer.println(String.format("%s;%s", result[0], result[1]));
-                                result[0] = null;
-                            }
-                        }
-                    });
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] temp = line.split(" ");
+                String status = temp[0];
+                String date = temp[1];
+                if (Objects.equals(status, "400") || Objects.equals(status, "500")) {
+                    if (Objects.isNull(result[0])) {
+                        result[0] = date;
+                    }
+                } else {
+                    if (Objects.nonNull(result[0])) {
+                        result[1] = date;
+                        writer.println(String.format("%s;%s", result[0], result[1]));
+                        result[0] = null;
+                    }
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
