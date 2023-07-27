920c27b802cb758c45986ae644c000b383e60c16
dev-suleymanov <dev.suleymanov@gmail.com>
2023-07-27 15:37:18 +0300

    12. Валидация параметров запуска [#246865 #432604]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -15,17 +15,20 @@ public class Search {
     }
 
     public static void validate(String[] args) {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("There should be two arguments");
-        }
         if (!Files.isDirectory(Paths.get(args[0]))) {
             throw new IllegalArgumentException("Set the initial path");
         }
         if (!args[1].startsWith(".")) {
             throw new IllegalArgumentException("The file extension must start with \".\"");
         }
+        if (args[1].length() < 3) {
+            throw new IllegalArgumentException("The length of the extension must be at least three characters");
+        }
     }
     public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("There should be two arguments");
+        }
         validate(args);
         search(Paths.get(args[0]), p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
