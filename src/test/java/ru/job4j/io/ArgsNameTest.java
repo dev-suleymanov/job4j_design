d32afbc4a2b70588d4251a2b336e0c196b0a497c
dev-suleymanov <dev.suleymanov@gmail.com>
2023-07-28 19:06:25 +0300

    13. Именованные аргументы [#295518 # [#295518 #432960]]

--- src/test/java/ru/job4j/io/ArgsNameTest.java
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -1,30 +1,33 @@
 package ru.job4j.io;
+
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ArgsNameTest {
 
     @Test
-    void whenGetFirst() {;
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+    void whenGetFirst() {
+        ;
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
         assertThat(jvm.get("Xmx")).isEqualTo("512");
     }
 
     @Test
     void whenGetFirstReorder() {
-        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        ArgsName jvm = ArgsName.of(new String[]{"-encoding=UTF-8", "-Xmx=512"});
         assertThat(jvm.get("Xmx")).isEqualTo("512");
     }
 
     @Test
     void whenMultipleEqualsSymbol() {
-        ArgsName jvm = ArgsName.of(new String[] {"-request=?msg=Exit="});
+        ArgsName jvm = ArgsName.of(new String[]{"-request=?msg=Exit="});
         assertThat(jvm.get("request")).isEqualTo("?msg=Exit=");
     }
 
     @Test
     void whenKeyNotExist() {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512"});
         assertThatThrownBy(() -> jvm.get("Xms")).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageMatching("^.+")
                 .hasMessageContaining("This key: 'Xms' is missing");
