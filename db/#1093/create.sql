8c26d9972bc9cce4b7cb00ac5430c12ce8bd2fdd
dev-suleymanov <dev.suleymanov@gmail.com>
2023-08-26 19:44:55 +0300

    2. Схема прав пользователи и роли. [#1093 #439904]

--- db/#1093/create.sql
+++ db/#1093/create.sql
@@ -1,35 +1,3 @@
-create table users(
-	id serial primary key,
-	name varchar(32),
-	role_id int references roles(id),
-	item_id int references items(id)
-);
-
-create table roles(
-	id serial primary key, 
-	name varchar(32)
-);
-
-create table rules(
-	id serial primary key, 
-	name varchar(32)
-);
-
-create table roles_rules(
-	id serial primary key, 
-	role_id int references roles(id),
-	rule_id int references rules(id)
-);
-
-create table items(
-	id serial primary key, 
-	name varchar(32),
-	comment_id int references comments(id),
-	attach_id int references attachs(id),
-	categorie_id int references categories(id),
-	state_id int references states(id)
-);
-
 create table comments(
 	id serial primary key, 
 	name text
@@ -48,4 +16,36 @@ create table categories(
 create table states(
 	id serial primary key,
 	name varchar(32)
+);
+
+create table items(
+	id serial primary key, 
+	name varchar(32),
+	comment_id int references comments(id),
+	attach_id int references attachs(id),
+	categorie_id int references categories(id),
+	state_id int references states(id)
+);
+
+create table roles(
+	id serial primary key, 
+	name varchar(32)
+);
+
+create table rules(
+	id serial primary key, 
+	name varchar(32)
+);
+
+create table roles_rules(
+	id serial primary key, 
+	role_id int references roles(id),
+	rule_id int references rules(id)
+);
+
+create table users(
+	id serial primary key,
+	name varchar(32),
+	role_id int references roles(id),
+	item_id int references items(id)
 );
