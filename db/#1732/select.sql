bb0ed1867955fa8c04a26f1e48e9e09be51dfd50
dev-suleymanov <dev.suleymanov@gmail.com>
2023-08-30 10:07:39 +0300

    3. Фильтры [#1732 #440847]

--- db/#1732/select.sql
+++ db/#1732/select.sql
@@ -7,6 +7,10 @@ from product p where p.name like '%мороженное%';
 select p.name "Название", p.expired_date "Дата", p.price "Цена" 
 from product p where p.expired_date < current_date;
 
+select p.name "Название", p.expired_date "Срок", p.price "Цена", t.name "Тип"
+from product p join type t on p.type_id = t.id
+where p.price = (select max(price) from product);
+
 select t.name "Тип", count(t.name) "Кол-во" 
 from product p join type t on p.type_id = t.id group by t.name;
 
