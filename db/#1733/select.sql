367d6b1fcc4f55c9ca41b3ee2110ab66654c37d3
dev-suleymanov <dev.suleymanov@gmail.com>
2023-08-31 12:21:44 +0300

    1. Хранилище машин [#1733 #441149]

--- db/#1733/select.sql
+++ db/#1733/select.sql
@@ -1,3 +1,8 @@
+select c.name "Машина", b.name "Кузов", e.name "Двигатель", t.name "Коробка передач" from car c 
+join car_bodies b on c.body_id = b.id 
+join car_engines e on c.engine_id = e.id
+join car_transmissions t on c.transmission_id = t.id;
+
 select b.name "Кузова" from car_bodies b 
 left join car c on b.id = c.body_id 
 where c.body_id is null;
