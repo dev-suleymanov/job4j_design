a664919e326eea0616bdf3fab55da7adfa2a6289
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-05 17:35:55 +0300

    Update all SQL - files

---
+++ db/3. Фильтры [#1732 #440847]/select.sql
@@ -0,0 +1,24 @@
+select p.name "Название", p.expired_date "Срок", p.price "Цена" 
+from product p join type t on p.type_id = t.id where t.name = 'СЫР'; 
+
+select p.name "Название", p.expired_date "Срок", p.price "Цена" 
+from product p where p.name like '%мороженное%';
+
+select p.name "Название", p.expired_date "Дата", p.price "Цена" 
+from product p where p.expired_date < current_date;
+
+select p.name "Название", p.expired_date "Срок", p.price "Цена", t.name "Тип"
+from product p join type t on p.type_id = t.id
+where p.price = (select max(price) from product);
+
+select t.name "Тип", count(t.name) "Кол-во" 
+from product p join type t on p.type_id = t.id group by t.name;
+
+select p.name "Название", p.expired_date "Дата", p.price "Срок" 
+from product p join type t on p.type_id = t.id 
+where t.name = 'СЫР' or t.name = 'МОЛОКО';
+
+select t.name "Тип" from type t group by t.name having count(t.name) < 10;
+
+select p.name "Название", p.expired_date "Срок", p.price "Цена", t.name "Тип"
+from product p join type t on p.type_id = t.id; 
