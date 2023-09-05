4f020148f15289f2e843c4efce0535eed61192ff
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-05 17:35:44 +0300

    Update all SQL - files

--- db/#373617/select.sql
+++
@@ -1,8 +0,0 @@
-select s.name, s.surname, ss.number
-from students as s join snilses as ss on s.snils_id = ss.id;
-
-select s.name as Имя, s.surname as Фамилия, ss.number as Снилс
-from students as s join snilses as ss on s.snils_id = ss.id;
-
-select s.name Имя, s.surname Фамилия, ss.number Снилс
-from students s join snilses ss on s.snils_id = ss.id;
--- db/#6861/insert.sql
+++
@@ -1,3 +0,0 @@
-insert into fauna(name, avg_age, discovery_date) values('cat', 15000, '19.05.1923');
-insert into fauna(name, avg_age, discovery_date) values('dog', 20000, '09.01.1911');
-insert into fauna(name, avg_age, discovery_date) values('paik-fish', 30000, '25.02.1965');
