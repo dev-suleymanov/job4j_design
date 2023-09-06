45e267f3954305efceb8ea3a720906f0f8fda500
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-06 15:32:29 +0300

    3. Хранимые процедуры и функции [#504804]

---
+++ db/3. Хранимые процедуры и функции [#504804]/insert.sql
@@ -0,0 +1,3 @@
+insert into products (name, producer, count, price) VALUES ('Danone', 'Actimel', 10, 100);
+insert into products (name, producer, count, price) VALUES ('Mars', 'Snickers', 14, 50);
+insert into products (name, producer, count, price) VALUES ('Danone', 'Danissimo', 28, 30);
