1890cf757c22c37060984237f2501699bb3f72cf
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-06 19:39:22 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ db/4.3. Транзакции. Уровни изолированности транзакций. [#504812]/insert.sql
@@ -0,0 +1,3 @@
+insert into students(name, surname, age) values('Ivan', 'Ivanov', 19);
+insert into students(name, surname, age) values('Maxim', 'Maximov', 20);
+insert into students(name, surname, age) values('Petr', 'Petrov', 18);
---
+++ db/4.3. Транзакции. Уровни изолированности транзакций. [#504812]/update.sql
@@ -0,0 +1,2 @@
+update students set age = 26 where name = 'Maxim';
+update students set age = 18 where name = 'Ivan';
