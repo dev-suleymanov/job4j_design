c6d52b1db07a7097d892929895f114557eedf3e1
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-07 22:08:10 +0300

    5. Курсор в SQL [#504816 #443335]

--- db/5. Курсор в SQL [#504816]/cursor.sql
+++ db/5. Курсор в SQL [#504816]/cursor.sql
@@ -3,13 +3,10 @@ declare cursor_products scroll cursor for select * from products;
 
 fetch last from cursor_products;
 move backward 6 FROM cursor_products;
-
 fetch from cursor_products;
 move backward 9 FROM cursor_products;
-
 fetch from cursor_products;
 move backward 6 FROM cursor_products;
-
 fetch from cursor_products;
 fetch prior from cursor_products;
 
