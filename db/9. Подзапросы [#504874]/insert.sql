bc7b12243e45d47230d0f8089699eeaddb80843f
dev-suleymanov <dev.suleymanov@gmail.com>
2023-09-12 18:19:41 +0300

    9. Подзапросы [#504874]

---
+++ db/9. Подзапросы [#504874]/insert.sql
@@ -0,0 +1,17 @@
+insert into customers(first_name, last_name, age, country) 
+values ('Ivan', 'Ivanov', 25, 'Anapa');
+insert into customers(first_name, last_name, age, country) 
+values ('Petr', 'Petrov', 42, 'Vladimir');
+insert into customers(first_name, last_name, age, country) 
+values ('Oleg', 'Lomov', 19, 'Dubna');
+insert into customers(first_name, last_name, age, country) 
+values ('Inna', 'Izmaylova', 26, 'Kaluga');
+insert into customers(first_name, last_name, age, country) 
+values ('Elena', 'Lastova', 18, 'Moscow');
+
+insert into orders(amount, customer_id) values(1150, 1);
+insert into orders(amount, customer_id) values(760, 2);
+insert into orders(amount, customer_id) values(255, 1);
+insert into orders(amount, customer_id) values(514, 1);
+insert into orders(amount, customer_id) values(1765, 5);
+insert into orders(amount, customer_id) values(5612, 2);
